7c05e2d1620f2f2c2481c9123b8606f8b43b119d
entremotivator <104661532+entremotivator@users.noreply.github.com>
2025-08-26 07:41:15 -0700

    Update 1_🤓_Homepage.py

--- 1_🤓_Homepage.py
+++ 1_🤓_Homepage.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import requests
-import datetime
+from datetime import datetime, timezone
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
